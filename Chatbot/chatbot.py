2db7f62c6b2a62b39d90d4e29a38a26414f9a3d4
Pratik Daherwal <38158708+yesiampratik@users.noreply.github.com>
2021-08-18 12:55:58 +0530

    Add files via upload

---
+++ Chatbot/chatbot.py
@@ -0,0 +1,56 @@
+import time
+print("Hello")
+time.sleep(1)
+print("And Byee")
+time.sleep(5)
+print("")
+print("")
+time.sleep(2)
+print("Just kidding")
+time.sleep(1)
+print("HEllo!! My Name is Justine")
+time.sleep(1)
+Name=input("Whats your Name?? :")
+time.sleep(1)
+gender=input("Are u a Boy or Girl :")
+A2=gender
+if A2=="Boy" or A2=="boy":
+  print("Oh hello Mr." ,Name,"Nice to meet u")
+elif A2=="Girl" or A2=="girl":
+  print("Oh hello Miss." ,Name,"Nice to meet u")
+else:
+  print("Error")
+time.sleep(1)
+year=int(input("What year is this going on?(only 4 digit numbers) :"))
+time.sleep(2)
+Dob=int(input("In which year u have born(only 4 digit number)? :"))
+time.sleep(2)
+print(" So, your Age is",year-Dob,"")
+time.sleep(2)
+print("so,you will complete 1 century in" ,Dob+100,"")
+time.sleep(1)
+Hero=input("Your favourite Avengers Among (Thor or Ironman)")
+A=Hero.lower()
+if Hero=="Thor":
+  print("But I like Both")
+elif Hero=="Ironman":
+  print("But I like Both")
+else:
+  print("Ooo")
+Movie=input("Your Favourite Movie?:")
+time.sleep(1)
+print("Ooo",Movie,"I will see this Movie")
+time.sleep(1)
+Anime=input("Your Favourite Anime?(Demon Slayer or Naruto)")
+if Anime=="demon slayer" or Anime=="Demon slayer" or Anime=="Demon Slayer":
+  print("I love Demon Slayer so much I think Tanjiro will take all Kizukis ")
+elif Anime=="Naruto" or Anime=="naruto":
+  print("I know It's your Favourite bcoz Naruto is so Funny and he has the 9 Tails in him")
+
+else:
+  print("Oh yes! I Also know this Cartoon")
+time.sleep(1)  
+print("OK Byee",Name,"And Nice to talk with you ")
+time.sleep(1)
+
+ 
